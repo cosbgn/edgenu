1103c2148c50e5163121aa6dda6e2cc53e0c6788
Costantin Bignoli <cosbgn@gmail.com>
2023-06-09 19:58:02 +0300

    ts

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -5,13 +5,11 @@ import * as schema from "../db/schema.js"
 // const client = createPool({maxUses:1, connectionString: process.env.POSTGRES_URL})
 
 let _db = null
-const useDb = async () => {
+export async function useDb () {
 	if (!_db){
 		const client = createClient({maxUses:1, connectionString: process.env.POSTGRES_URL_NON_POOLING})
 		await client.connect()
-		_db = drizzle(client, {schema}) 
+		_db = drizzle(client, {schema})
 	}
 	return _db
 }
-
-module.exports = useDb
