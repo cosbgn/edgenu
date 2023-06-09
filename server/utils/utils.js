aae7b305ecd1eca48a2bcd0ec60dcf8276ef95de
Costantin Bignoli <cosbgn@gmail.com>
2023-06-09 14:26:48 +0300

    testing constructor bug

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -6,7 +6,11 @@ let _db = null
 export const useDb = () => {
 	if (!_db){
 		// Max 1 is needed on edge functions
-		const client = postgres(process.env.POSTGRES_URL, { max: 1, ssl:'require' });
+		const client = new postgres(process.env.POSTGRES_URL, { 
+			max: 1, 
+			ssl:'require',
+			connect_timeout: 10,
+		});
 		_db = drizzle(client, { schema })
 	}
 	return _db
