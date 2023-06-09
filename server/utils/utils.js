3a2f161a639ef8825cbb2a18d8b2f9169afdd2da
Costantin Bignoli <cosbgn@gmail.com>
2023-06-09 15:39:17 +0300

    test

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -5,6 +5,11 @@ import { drizzle } from 'drizzle-orm/neon-serverless'
 import * as schema from "../db/schema.js"
 let _db = null
 export const useDb = () => {
+	console.log(`Env is: ${process.env.NODE_ENV}`)
+	// https://github.com/neondatabase/serverless#run-on-node
+	if (process.env.NODE_ENV === "development"){
+		neonConfig.webSocketConstructor = ws
+	}
 	if (!_db){
 		// Max 1 is needed on edge functions neonConfig.webSocketConstructor = ws; 
 		const pool = new Pool({ connectionString: process.env.POSTGRES_URL })
