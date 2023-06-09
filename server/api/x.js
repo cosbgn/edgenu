8af472144c356889927bd18375794d4347c5cc63
Costantin Bignoli <cosbgn@gmail.com>
2023-06-09 15:15:33 +0300

    test pg

--- server/api/x.js
+++ server/api/x.js
@@ -1,10 +1,18 @@
 
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
+import pkg from 'pg';
+const { Pool } = pkg;
+
 import {users} from "../db/schema.js"
-const db = drizzle(postgres(process.env.POSTGRES_URL, {ssl:'require'}))
+
+const pool = new Pool({
+	connectionString: process.env.POSTGRES_URL,
+	ssl: { rejectUnauthorized: false}
+})
+const db = drizzle(pool)
 
 export default defineEventHandler(async (event) => {
 	const x = await db.select().from(users)
-	return "Works"
+	return x
 })
