21dbcd7842a7d8bedfb686118fb5e0af64cb03c9
Costantin Bignoli <cosbgn@gmail.com>
2023-11-10 19:19:00 +0000

    added mid example

--- server/api/form-test.post.js
+++ server/api/form-test.post.js
@@ -1,6 +1,9 @@
 export default defineEventHandler(async (event) => {
-	console.log("works")
-	const form = await readFormData(event)
-	console.log("doesn't work")
+	// console.log("works")
+	// const form = await readFormData(event)
+	// console.log(form)
+	const raw = await readBody(event)
+	console.log(raw)
+	// console.log("doesn't work")
 	return "DONE"
 })
